e86a67a329b575a9b646388564ff1916d4f2e387
N0ku <lucielecouturier2@gmail.com>
2022-10-03 21:10:08 +0200

    Push discord bot on git

---
+++ main.js
@@ -0,0 +1,32 @@
+const Discord = require("Discord.js")
+require("dotenv").config();
+
+const client = new Discord.Client({
+    intents: [
+        Discord.GatewayIntentBits.Guilds/* ,
+        Discord.GatewayIntentBits.GuildMessages,
+        Discord.GatewayIntentBits.MessageContent,
+        Discord.GatewayIntentBits.GuildMembers, */
+    ]
+});
+
+client.once("ready", () => {
+    console.log("Tsuki is now online.");
+});
+
+let channelIds = [
+    "584168184152850454",
+    "586876967177945109",
+    "720436686944993421"
+];
+
+function sendMsg() {
+    channelIds.forEach(id => {
+        const channel = client.channels.cache.get(id);
+        channel.send("Test Channel");
+    });
+}
+
+setInterval(sendMsg, 2000);
+
+client.login(process.env.TOKEN);
